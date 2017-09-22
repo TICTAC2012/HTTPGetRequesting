9f8bb55e366c53e94555755a1d196b7b708d0a9f
TICTAC2012 <tremainecrawford@googlemail.com>
2017-09-22 19:59:04 +0100

    Output and Errors
    Allows user to enter a line and have it output by the program.

--- src/main/HTTPRequesting.java
+++ src/main/HTTPRequesting.java
@@ -13,8 +13,19 @@ public class HTTPRequesting {
 		scanner.close();
 	}
 	
+	//Gives input to program and output what was entered.
 	public static void writeOutput() {
-		
+		try {
+			Scanner scanner = new Scanner(System.in);
+			if(scanner.hasNextLine()) {
+				String stringUrl = scanner.nextLine();
+				System.out.println(stringUrl);
+			}
+			scanner.close();
+		}
+		catch(RuntimeException e) {
+			System.err.println("Output could not be written to command line");
+		}
 	}
 	
 	
--- src/test/Test1.java
+++ src/test/Test1.java
@@ -4,13 +4,23 @@ import org.junit.Test;
 
 import main.HTTPRequesting;
 
-//Test 1 - The program takes arguments from the command line and consumes input from stdin.
 public class Test1 {
 	
+	//Test 1 - The program takes arguments from the command line and consumes input from stdin.
 	@Test
 	public final void whenInputIsGivenFromStdin() {
 		HTTPRequesting.catchInput();
 	}
 	
+	//Test 2 - The program output is written to stdout and errors are written to stderr.
+	@Test
+	public final void whenOutputOfArgumentsFromStdinIsPrinted() {
+		HTTPRequesting.writeOutput();
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public final void whenAnErrorOccurs() {
+		HTTPRequesting.writeOutput();
+	}
 
 }
